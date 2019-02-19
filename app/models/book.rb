5467a9310442007b3f217759ddcea1cbaf6fc74d
Jose Manuel Gomez <jmgomezr@gmail.com>
2019-02-19 17:36:26 +0100

    Definida has many through association
    En modelos Book, User y Borrow

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ApplicationRecord
   has_many_attached :fotos
+
+  has_many :borrows
+  has_many :users, through: :borrows
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :borrows
+  has many :books, through: :borrows
 end
